91f971845c30b7924944d7838c7d75e292ecdc16
skypjack <michele.caini@gmail.com>
2026-05-05 14:33:40 +0200

    stl: allow injecting array

--- test/entt/stl_ext/array.cpp
+++ test/entt/stl_ext/array.cpp
@@ -1,3 +1,6 @@
-#include <memory>
 #include <gtest/gtest.h>
 #include <entt/stl/array.hpp>
+
+TEST(Array, HasInclude) {
+    static_assert(entt::stl::entt_ext_array, "Header not properly included");
+}

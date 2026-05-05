90b09969f254c4583255dcd0cd7b5ec5b50507f5
skypjack <michele.caini@gmail.com>
2026-05-05 14:38:34 +0200

    stl: allow injecting bit

--- src/entt/stl/bit.hpp
+++ src/entt/stl/bit.hpp
@@ -1,9 +1,12 @@
 #ifndef ENTT_STL_BIT_HPP
 #define ENTT_STL_BIT_HPP
 
-#include <bit>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/bit.hpp>)
+#    include <entt/ext/stl/bit.hpp>
+#else
+#    include <bit>
+
 namespace entt::stl {
 
 using std::bit_ceil;
@@ -11,6 +14,7 @@ using std::has_single_bit;
 using std::popcount;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif

e0ce66c098618b3338229b93dda67749153832b8
skypjack <michele.caini@gmail.com>
2026-05-06 14:57:33 +0200

    stl: allow injecting cstdint

--- src/entt/stl/cstdint.hpp
+++ src/entt/stl/cstdint.hpp
@@ -1,9 +1,12 @@
 #ifndef ENTT_STL_CSTDINT_HPP
 #define ENTT_STL_CSTDINT_HPP
 
-#include <cstdint>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/cstdint.hpp>)
+#    include <entt/ext/stl/cstdint.hpp>
+#else
+#    include <cstdint>
+
 namespace entt::stl {
 
 using std::uint16_t;
@@ -12,6 +15,7 @@ using std::uint64_t;
 using std::uint8_t;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif

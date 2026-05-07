7d5ce4ec3e416ae19fb6085e1cca507042d54dea
skypjack <michele.caini@gmail.com>
2026-05-07 15:59:44 +0200

    stl: allow injecting ios

--- src/entt/stl/ios.hpp
+++ src/entt/stl/ios.hpp
@@ -1,14 +1,18 @@
 #ifndef ENTT_STL_IOS_HPP
 #define ENTT_STL_IOS_HPP
 
-#include <ios>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/ios.hpp>)
+#    include <entt/ext/stl/ios.hpp>
+#else
+#    include <ios>
+
 namespace entt::stl {
 
 using std::boolalpha;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif

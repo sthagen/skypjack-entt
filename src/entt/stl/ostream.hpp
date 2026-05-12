e45a3f512dd9263e8245782c53b91865db924177
skypjack <michele.caini@gmail.com>
2026-05-12 17:07:48 +0200

    stl: allow injecting ostream

--- src/entt/stl/ostream.hpp
+++ src/entt/stl/ostream.hpp
@@ -1,14 +1,18 @@
 #ifndef ENTT_STL_OSTREAM_HPP
 #define ENTT_STL_OSTREAM_HPP
 
-#include <ostream>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/ostream.hpp>)
+#    include <entt/ext/stl/ostream.hpp>
+#else
+#    include <ostream>
+
 namespace entt::stl {
 
 using std::ostream;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif

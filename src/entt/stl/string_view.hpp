3e660d65e83e5ed9f06b77d19fd070973dfc83ad
skypjack <michele.caini@gmail.com>
2026-05-12 18:07:44 +0200

    stl: support injecting string_view

--- src/entt/stl/string_view.hpp
+++ src/entt/stl/string_view.hpp
@@ -1,15 +1,19 @@
 #ifndef ENTT_STL_STRING_VIEW_HPP
 #define ENTT_STL_STRING_VIEW_HPP
 
-#include <string_view>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/string_view.hpp>)
+#    include <entt/ext/stl/string_view.hpp>
+#else
+#    include <string_view>
+
 namespace entt::stl {
 
 using std::string_view;
 using std::swap;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif

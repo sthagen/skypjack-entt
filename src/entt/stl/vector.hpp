a99ad7eddb305bd0b420a9ca1f1d332116338e37
skypjack <michele.caini@gmail.com>
2026-05-13 09:47:37 +0200

    stl: support injecting vector

--- src/entt/stl/vector.hpp
+++ src/entt/stl/vector.hpp
@@ -1,14 +1,18 @@
 #ifndef ENTT_STL_VECTOR_HPP
 #define ENTT_STL_VECTOR_HPP
 
-#include <vector>
-
 /*! @cond ENTT_INTERNAL */
+#if __has_include(<entt/ext/stl/vector.hpp>)
+#    include <entt/ext/stl/vector.hpp>
+#else
+#    include <vector>
+
 namespace entt::stl {
 
 using std::vector;
 
 } // namespace entt::stl
+#endif
 /*! @endcond */
 
 #endif
--- test/entt/stl_ext/vector.cpp
+++ test/entt/stl_ext/vector.cpp
@@ -1,2 +1,6 @@
 #include <gtest/gtest.h>
 #include <entt/stl/vector.hpp>
+
+TEST(Vector, HasInclude) {
+    static_assert(entt::stl::entt_ext_vector, "Header not properly included");
+}

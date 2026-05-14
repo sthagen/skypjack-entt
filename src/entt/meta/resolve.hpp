f1a70e4d939a58e3e1db08d2d37368c032bd409a
skypjack <michele.caini@gmail.com>
2026-05-14 15:57:01 +0200

    meta: internal changes

--- src/entt/meta/resolve.hpp
+++ src/entt/meta/resolve.hpp
@@ -38,7 +38,7 @@ template<typename Type>
  * @param ctx The context from which to search for meta types.
  * @return An iterable range to use to visit all meta types.
  */
-[[nodiscard]] inline meta_range<meta_type, typename internal::meta_context::container_type::const_iterator> resolve(const meta_ctx &ctx) noexcept {
+[[nodiscard]] inline meta_range<meta_type, typename internal::meta_context::bucket_type::const_iterator> resolve(const meta_ctx &ctx) noexcept {
     const auto &context = internal::meta_context::from(ctx);
     return {{ctx, context.bucket.cbegin()}, {ctx, context.bucket.cend()}};
 }
@@ -47,7 +47,7 @@ template<typename Type>
  * @brief Returns a range to use to visit all meta types.
  * @return An iterable range to use to visit all meta types.
  */
-[[nodiscard]] inline meta_range<meta_type, typename internal::meta_context::container_type::const_iterator> resolve() noexcept {
+[[nodiscard]] inline meta_range<meta_type, typename internal::meta_context::bucket_type::const_iterator> resolve() noexcept {
     return resolve(locator<meta_ctx>::value_or());
 }
 
